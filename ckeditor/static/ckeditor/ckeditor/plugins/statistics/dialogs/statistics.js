1c5a22c99effd33439dce41979ac821d4e2c32ce
Mohamed Ali AFFES <mohamed.ali.affes@gmail.com>
2014-12-11 17:03:22 +0100

    Trying easyChart js lib

--- ckeditor/static/ckeditor/ckeditor/plugins/statistics/dialogs/statistics.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/statistics/dialogs/statistics.js
@@ -6,76 +6,130 @@
 (function(){CKEDITOR.dialog.add("statistics",function(c){
 	function o(a,b){var k=CKEDITOR.dom.element.createFromHtml('<a href="javascript:void(0)" tabIndex="-1" role="option" ><div class="cke_tpl_item"></div></a>'),d='<table style="width:350px;" class="cke_tpl_preview" role="presentation"><tr>';a.image&&b&&(d+='<td class="cke_tpl_preview_img"><img src="'+CKEDITOR.getUrl(b+a.image)+'"'+(CKEDITOR.env.ie6Compat?' onload="this.width=this.width"':"")+' alt="" title=""></td>');d+='<td style="white-space:normal;"><span class="cke_tpl_title">'+
 a.title+"</span><br/>";a.description&&(d+="<span>"+a.description+"</span>");k.getFirst().setHtml(d+"</td></tr></table>");k.on("click",function(){p(a.html)});return k}function p(a){
-	/*c.test=function () {
-			var dialog = CKEDITOR.dialog.getCurrent(),
-					data = {},
-					statistics = c.document.getById('myStat1');
-					//chart = editor.document.createElement( 'div' );
-				
-				// Populate the data object with data entered in the dialog window.
-				dialog.commitContent( data );
+	
+	c.test=function (a) {
+		c.insertHtml(a);
 
-				// Set the div element attributes.	
-				statistics.setAttribute( 'data-text',
-					data.part_value+'%'
-					//dialog.getValueOf( 'tab-basic', 'part-value' )+'%'
-					);
-				statistics.setAttribute( 'data-percent',
-					data.part_value
-					//dialog.getValueOf( 'tab-basic', 'part-value' )
-					);
-				var iframe = $('iframe'); $('#myStat1', iframe.contents()).circliful();
-		};*/
-	c.stat=a;	
-	var b=CKEDITOR.dialog.getCurrent();b.getValueOf("selectTpl"/*,"chkInsertOpt"*/)?(c.fire("saveSnapshot"),c.setData(a,function(){b.hide();var a=c.createRange();a.moveToElementEditStart(c.editable());a.select();setTimeout(function(){c.fire("saveSnapshot")},0)})):(c.insertHtml(a),b.hide())}
+		var dialog = CKEDITOR.dialog.getCurrent(),
+			data = {},
+			span = c.document.getById('chart');
+			//span = c.document.createElement(a);
+			//span = c.document.createElement( 'span' );
+			//innerSpan = c.document.createElement( 'span' );
+
+			dialog.commitContent( data );
+			span.setAttribute( 'data-percent', data.percentage );
+			
+			//span.append(innerSpan);
+			//c.insertElement( span );
+			dialog.hide();
+			
+			//span.setHtml( c.statHtml );
+//span.setHtml( a);
+			//span.setAttribute( 'class', 'chart')
+			//innerSpan.setAttribute( 'class', 'percentage')
+			
+			// Populate the data object with data entered in the dialog window.
+			/*dialog.commitContent( data );
+
+			span.setAttribute( 'data-percent',
+				data.percentage
+				//dialog.getValueOf( 'tab-basic', 'part-value' )
+				);*/
+			
+			
+			//var iframe = $('iframe');
+
+			//$('#myStat1', iframe.contents()).circliful();
+			//var b = $('.chart', iframe.contents());
+			
+			var iframe = document.getElementsByClassName('cke_wysiwyg_frame cke_reset')[0];
+			var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+			var chart = window.chart = new EasyPieChart(
+				innerDoc.querySelector('.chart'), {
+				easing: 'easeOutElastic',
+				delay: 3000,
+				onStep: function(from, to, percent) {
+					this.el.children[0].innerHTML = Math.round(percent);
+				}
+			});
+
+			/*document.querySelector('.js_update').addEventListener('click', function(e) {
+				chart.update(Math.random()*200-100);
+			});*/
+	};
+	var b=CKEDITOR.dialog.getCurrent();b.getValueOf("selectTpl"/*,"chkInsertOpt"*/)?(c.fire("saveSnapshot"),c.setData(a,function(){b.hide();var a=c.createRange();a.moveToElementEditStart(c.editable());a.select();setTimeout(function(){c.fire("saveSnapshot")},0)})):(/*c.insertHtml(a),*/c.test(a)/*,b.hide()*/)}
 function i(a){var b=a.data.getTarget(),
 c=g.equals(b);if(c||g.contains(b)){var d=a.data.getKeystroke(),f=g.getElementsByTag("a"),e;if(f){if(c)e=f.getItem(0);else switch(d){case 40:e=b.getNext();break;case 38:e=b.getPrevious();break;case 13:case 32:b.fire("click")}e&&(e.focus(),a.data.preventDefault())}}}
 var h=CKEDITOR.plugins.get("statistics");
+
 CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(h.path+"dialogs/statistics.css"));
-CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(h.path+"lib/jquery-plugin-circliful/css/jquery.circliful.css"));
+/*CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(h.path+"lib/jquery-plugin-circliful/css/jquery.circliful.css"));
 CKEDITOR.scriptLoader.load( CKEDITOR.getUrl(h.path+"lib/jquery-1.10.2.min.js"));
 CKEDITOR.scriptLoader.load( CKEDITOR.getUrl(h.path+"lib/jquery-plugin-circliful/js/jquery.circliful.min.js"));
+*/
+
+CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(h.path+"lib/easypiechart.css"));
+CKEDITOR.scriptLoader.load( CKEDITOR.getUrl(h.path+"lib/easypiechart.min.js"));
 
 var g,h="cke_tpl_list_label_"+CKEDITOR.tools.getNextNumber(),f=c.lang.statistics,l=c.config;return{title:c.lang.statistics.title,
-minWidth:CKEDITOR.env.ie?440:400,minHeight:340,contents:[{id:"selectTpl",label:f.title,elements:[{type:"vbox",padding:5,children:[{id:"selectTplText",type:"html",html:"<span>"+f.selectPromptMsg+"</span>"},{id:"statisticsList",type:"html",focus:!0,html:'<div class="cke_tpl_list" tabIndex="-1" role="listbox" aria-labelledby="'+h+'"><div class="cke_tpl_loading"><span></span></div></div><span class="cke_voice_label" id="'+h+'">'+f.options+"</span>"},
-/*{id:"chkInsertOpt",type:"checkbox",label:f.insertOption,
-"default":false
-//l.statistics_replaceContent},
- */
+minWidth:CKEDITOR.env.ie?440:400,minHeight:340,
+contents:[
+	{
+		id:"selectTpl",
+		label:f.title,
+		elements:[
+			{
+				type:"vbox",
+				/*padding:2,*/
+				children:[
+					{
+						type: 'text',
+						id: 'percentage',
+						label: 'Percentage',
+						required : true,
+						validate: CKEDITOR.dialog.validate.number( "Invalid number." ),
+						commit : function( data )
+						{
+							data.percentage = this.getValue();
+						}
+					},
+					{
+						type : 'text',
+						id : 'description',
+						label : 'Description',
+					//  validate : CKEDITOR.dialog.validate.notEmpty( 'The Description field cannot be empty.' ),
+						// Function to be run when the commitContent method of the parent dialog window is called.
+						commit : function( data )
+						{
+							data.description = this.getValue();
+						}
+					},
+					{
+						type:"html",
+						html:"<div style='height=10px'></div>",
+					},
+					{
+						id:"selectTplText",
+						type:"html",
+						html:"<span>"+f.selectPromptMsg+"</span>"
+					},
+					{
+						id:"statisticsList",
+						type:"html",
+						focus:!0,
+						html:'<div class="cke_tpl_list" tabIndex="-1" role="listbox" aria-labelledby="'+h+'"><div class="cke_tpl_loading"><span></span></div></div><span class="cke_voice_label" id="'+h+'">'+f.options+"</span>"
+					},
+					/*{
+						id:"chkInsertOpt",
+						type:"checkbox",
+						label:f.insertOption,
+						"default":false
+						//l.statistics_replaceContent
+					},*/
+					 
  
- {
-	type: 'text',
-	id: 'part_value',
-	label: 'Value',
-	validate: CKEDITOR.dialog.validate.number( "Invalid number." ),
-	commit : function( data )
-	{
-		data.part_value = this.getValue();
-	}
-},
-{
-	type: 'text',
-	id: 'total_value',
-	label: 'Total',
-	//validate: CKEDITOR.dialog.validate.notEmpty( "Total field cannot be empty." )
-	commit : function( data )
-	{
-		data.total_value = this.getValue();
-	}
-},
-{
-	type : 'textarea',
-	id : 'description',
-	label : 'Description',
-//						validate : CKEDITOR.dialog.validate.notEmpty( 'The Description field cannot be empty.' ),
-	// This field is required.
-	required : true,
-	// Function to be run when the commitContent method of the parent dialog window is called.
-	commit : function( data )
-	{
-		data.description = this.getValue();
-	}
-},
 
 
 ]}]}],buttons:[CKEDITOR.dialog.cancelButton,CKEDITOR.dialog.okButton],
@@ -97,16 +151,28 @@ onOk: function() {
 
 				// Set the div element attributes.	
 				statistics.setAttribute( 'data-text',
-					data.part_value+'%'
+					data.percentage+'%'
 					//dialog.getValueOf( 'tab-basic', 'part-value' )+'%'
 					);
 				statistics.setAttribute( 'data-percent',
-					data.part_value
+					data.percentage
 					//dialog.getValueOf( 'tab-basic', 'part-value' )
 					);
-
+				
 				c.insertElement( statistics );
 
-				var iframe = $('iframe'); $('#myStat1', iframe.contents()).circliful();
+				//var iframe = $('iframe'); $('#myStat1', iframe.contents()).circliful();
+
+				var chart = window.chart = new EasyPieChart(document.querySelector('span'), {
+					easing: 'easeOutElastic',
+					delay: 3000,
+					onStep: function(from, to, percent) {
+						this.el.children[0].innerHTML = Math.round(percent);
+					}
+				});
+
+				document.querySelector('.js_update').addEventListener('click', function(e) {
+					chart.update(Math.random()*200-100);
+				});
 			}
 }})})();
--- ckeditor/static/ckeditor/ckeditor/plugins/statistics/statistics/default.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/statistics/statistics/default.js
@@ -9,12 +9,13 @@ CKEDITOR.addStats(
 		statistics:
 		[
 			{
-				title:"Circle 1",
+				title:"Circle chart",
 				image:"stat1.png",
 				description:"Circle chart with percentage value and simple description.",
-				html:'<div id="myStat1" data-dimension="100" data-text="75%" data-width="10" data-fontsize="12" data-percent="75" data-fgcolor="#61a9dc" data-bgcolor="#eee" ><div>Text</div>'
+				html:'<span id="chart" class="chart" data-percent="10"><span class="percent">&nbsp;</span></span>'
+
 			},
-			{
+			/*{
 				title:"Circle 2",
 				image:"stat2.png",
 				description:"Circle chart with percentage value, info-text and simple text.",
@@ -24,8 +25,8 @@ CKEDITOR.addStats(
 				title:"Circle 3",
 				image:"stat3.png",
 				description:"Circle chart with value info-text and fontawesome icon.",
-				html:'<div id="myStat4" data-dimension="100" data-text="72" data-info="New Users" data-width="10" data-fontsize="12" data-percent="46" data-fgcolor="#61a9dc" data-bgcolor="#eee" data-total="150" data-part="72" data-border="inline" data-icon="users" data-icon-size="28" data-icon-color="#ccc"></div>'
-			}
+				html:'<span class="chart" data-percent="86"><span class="percent"></span></span>'
+			}*/
 		]
 	}
 	);
