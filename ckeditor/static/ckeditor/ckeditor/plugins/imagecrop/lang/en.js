a8e3ac42b74422c4c5a68c184cd31c1ac0ee0257
mohamed-ali-affes <mohamed.ali.affes@gmail.com>
2015-01-15 08:51:38 +0100

    Integrating imagecrop feature

---
+++ ckeditor/static/ckeditor/ckeditor/plugins/imagecrop/lang/en.js
@@ -0,0 +1,31 @@
+﻿CKEDITOR.plugins.setLang( "imagecrop", "en",
+	{
+		menu	: "Crop the image",
+		title	: "Image cropping",
+
+		zoomIn		: "Zoom in",
+		zoomOut		: "Zoom out",
+		originalImage	: "Original Image",
+		fileSize		: "File Size",
+
+		targetImage : "Target Image",
+		chooseSizeInstructions	: "Pick how do you want to use this image:",
+		croppingInstructions	: "Move and resize the rectangle to select the area that you want to keep",
+		formatInstructions		: "Select the format and click Save:",
+
+		back	: "Back",
+		applyCrop : "Crop",
+		chooseFormat	: "Format:",
+		save	: "Save",
+
+		scale	: "(scale {0})",
+
+		imageTooSmall : "The image doesn't need to be cropped to fit it {0}x{1}.",
+		imageTooNarrow : "The image is too narrow.\r\nYou only have to crop its height.",
+		imageTooShort : "The image is too short.\r\You only have to crop its width.",
+
+		mustSelectCrop : "You must select the cropping area",
+
+		processing : "Processing..."
+	}
+);
